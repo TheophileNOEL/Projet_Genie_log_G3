a89b06a0b8ff96f0d957899f7aaf0559d22ea292
TheophileNOEL <theophile.noel@viacesi.fr>
2025-05-07 13:52:30 +0200

    Création de la classe Langue, pour définir la destination de toutes les langues.

---
+++ EasySave_G3_V1/Langue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave_G3_V1
+{
+    public class Langue
+    {
+        private string Title;
+        private string Source;
+
+        public Langue(string title, string source)
+        {
+            Title = title;
+            Source = source;
+        }
+        public string GetTitle()
+        {
+            return Title;
+        }
+        public string GetSource()
+        {
+            return Source;
+        }
+        public void SetTitle(string title)
+        {
+            Title = title;
+        }
+        public void SetSource(string source)
+        {
+            Source = source;
+        }
+    }
+}
