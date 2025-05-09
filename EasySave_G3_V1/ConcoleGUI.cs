d68e8b5f606e914008638f5dc184502394e120e8
TheophileNOEL <theophile.noel@viacesi.fr>
2025-05-09 14:34:09 +0200

    Implémentation du changement de langues

--- EasySave_G3_V1/ConcoleGUI.cs
+++ EasySave_G3_V1/ConcoleGUI.cs
@@ -4,22 +4,62 @@ using System.IO;
 using System.Reflection;
 namespace EasySave_G3_V1;
 
-class Progamm
+class Programm
 {
     static void Main()
     {
+        Programm p = new Programm();
+        Langage langage = new Langage("French.json", "C:\\Users\\User\\OneDrive - Association Cesi Viacesi mail\\CESI\\FISE A3\\Genie logiciel\\Projet\\EasySave_G3_V1\\EasySave_G3_V1\\Langages\\French.json");
+        p.Start(langage);
+        
+    }
+    void Start(Langage Langue)
+    {
+        // Loading the French langage
         Langages ListLangage = new Langages();
         ListLangage.SearchLangages();
-        Console.WriteLine("Liste de langues :");
+        Langage L = new Langage();
         foreach (Langage l in ListLangage.GetListLangage())
         {
-            l.LoadLangage();
-            foreach (KeyValuePair<string,string> k in l.GetElements())
+            if (l.GetTitle() == Langue.GetTitle())
             {
-                Console.WriteLine(k.Key);
-                Console.WriteLine(k.Value);
+                l.LoadLangage();
+                L = l;
             }
         }
-        Console.WriteLine("________________");
+        Console.WriteLine(L.GetElements()["Separator"]);
+        Console.WriteLine(L.GetElements()["Welcome"]);
+        Console.WriteLine(L.GetElements()["SelectAction"]);
+        int result = int.Parse(Console.ReadLine());
+        switch (result)
+        {
+            case 1: SelectScript(L); break;
+            case 2: UpdateScript(L); break;
+            case 3: SelectLangage(L,ListLangage.GetListLangage()); break;
+            case 4:break;
+        }
+    }
+
+    void SelectScript(Langage L)
+    {
+        Console.WriteLine(L.GetElements()["Separator"]);
+    }
+
+    void UpdateScript(Langage L)
+    {
+        Console.WriteLine(L.GetElements()["Separator"]);
+    }
+    void SelectLangage(Langage L, List<Langage> listLangages) 
+    {
+        Console.WriteLine(L.GetElements()["Separator"]);
+        Console.WriteLine(L.GetElements()["ChangeLangages"]);
+        int i = 1;
+        foreach (Langage l in listLangages)
+        {
+            Console.WriteLine(i+"    "+l.GetTitle().Split(".")[0]);
+            i++;
+        }
+        int result = int.Parse(Console.ReadLine());
+        Start(listLangages[result-1]);
     }
 }
