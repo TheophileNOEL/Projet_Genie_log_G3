a52efdf6070b44478b2866e35b2522d944b0147b
TheophileNOEL <theophile.noel@viacesi.fr>
2025-05-09 11:59:27 +0200

    Création de la classe d'IHM console, création d'une méthode pour aller chercher les informations sur la langue et ajout d'éléments dans la langue Français.

---
+++ EasySave_G3_V1/ConcoleGUI.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+using System.Reflection;
+namespace EasySave_G3_V1;
+
+class Progamm
+{
+    static void Main()
+    {
+        Langages ListLangage = new Langages();
+        ListLangage.SearchLangages();
+        Console.WriteLine("Liste de langues :");
+        foreach (Langage l in ListLangage.GetListLangage())
+        {
+            l.LoadLangage();
+            foreach (KeyValuePair<string,string> k in l.GetElements())
+            {
+                Console.WriteLine(k.Key);
+                Console.WriteLine(k.Value);
+            }
+        }
+        Console.WriteLine("________________");
+    }
+}
--- EasySave_G3_V1/Langage.cs
+++ EasySave_G3_V1/Langage.cs
@@ -11,17 +11,20 @@ namespace EasySave_G3_V1
     {
         private string Title;
         private string Source;
+        private Dictionary<string, string> Elements;
 
         public Langage()
         {
             Title = string.Empty;
             Source = string.Empty;
+            Elements = new Dictionary<string, string>();
         }
 
         public Langage(string title, string source)
         {
             Title = title;
             Source = source;
+            Elements = new Dictionary<string, string>();
         }
         public string GetTitle()
         {
@@ -38,6 +41,30 @@ namespace EasySave_G3_V1
         public void SetSource(string source)
         {
             Source = source;
+        }
+        public Dictionary<string, string> GetElements()
+        { 
+            return Elements; 
+        }
+        public void AddElement(Dictionary<string, string> element)
+        {
+            foreach (var kvp in element)
+            {
+                this.Elements[kvp.Key] = kvp.Value;
+            }
+        }
+        public void LoadLangage()
+        {
+            try
+            {
+                string jsonContent = File.ReadAllText(GetSource());
+                Dictionary<string,string> messages = JsonSerializer.Deserialize<Dictionary<string,string>>(jsonContent);
+                this.AddElement(element: messages);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
