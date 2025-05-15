562b0bccbbd1c80d1994695bdff3be27fa007613
Romano Corentin <corentin.romano@viacesi.fr>
2025-05-15 11:54:37 +0200

    V1 without CryptoSoft

--- V1/V1.0/EasySave_G3_V1/obj/Debug/net8.0/EasySave_G3_V1.AssemblyInfo.cs
+++ V1/V1.0/EasySave_G3_V1/obj/Debug/net8.0/EasySave_G3_V1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasySave_G3_V1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2814093dd71d075332de8399dad47dbfb4b74d87")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasySave_G3_V1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasySave_G3_V1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
